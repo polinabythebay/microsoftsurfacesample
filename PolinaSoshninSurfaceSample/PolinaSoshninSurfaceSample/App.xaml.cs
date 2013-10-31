77ab96e44864826b3badaddb577501ab06556970
Polina Soshnin <psoshnin@wellesley.edu>
2013-10-30 20:25:58 -0400

    Update App.xaml.cs

--- PolinaSoshninSurfaceSample/PolinaSoshninSurfaceSample/App.xaml.cs
+++ PolinaSoshninSurfaceSample/PolinaSoshninSurfaceSample/App.xaml.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Windows;
 
-namespace PolinaSoshninHW2
+namespace PolinaSoshninSurfaceSample
 {
     /// <summary>
     /// Interaction logic for App.xaml
